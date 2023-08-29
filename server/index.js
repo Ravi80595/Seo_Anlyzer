921ef63a855e38e7645d51158fc811fc9bc52c06
Ravi sharma <rsharma80595@gmail.com>
2023-08-29 11:00:46 +0530

    url changes

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(express.json())
 
 // app.use(cors({
-//     origin: 'http://localhost:3000/' // Replace with your frontend domain
+//     origin: 'http://localhost:3000' // Replace with your frontend domain
 // }));
 
 
